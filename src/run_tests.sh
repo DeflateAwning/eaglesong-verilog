28360654d0d764d84c158c0b5f203b80aff57598
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2024-02-04 10:03:54 +0900

    Kill off bad-practice modules created at the start while I was figuring it out

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -12,20 +12,6 @@ VVP_RUN_LOG_FILENAME='/tmp/vvp_run_log.txt'
 # create empty run log
 date --iso-8601=seconds > $VVP_RUN_LOG_FILENAME
 
-echo "=== Building tb_eaglesong_bit_matrix"
-iverilog $IVERILOG_ARGS -o ./tb_eaglesong_bit_matrix.iv_sim.vvp ./rtl/eaglesong_bit_matrix.v ./tb/tb_eaglesong_bit_matrix.v
-echo "iverilog exit code: $?"
-echo "=== Running tb_eaglesong_bit_matrix"
-vvp ./tb_eaglesong_bit_matrix.iv_sim.vvp | tee -a $VVP_RUN_LOG_FILENAME
-echo "=== Done tb_eaglesong_bit_matrix"
-
-echo "=== Building tb_eaglesong_coefficients"
-iverilog $IVERILOG_ARGS -o ./tb_eaglesong_coefficients.iv_sim.vvp ./rtl/eaglesong_coefficients.v ./tb/tb_eaglesong_coefficients.v
-echo "iverilog exit code: $?"
-echo "=== Running tb_eaglesong_coefficients"
-vvp ./tb_eaglesong_coefficients.iv_sim.vvp | tee -a $VVP_RUN_LOG_FILENAME
-echo "=== Done tb_eaglesong_coefficients"
-
 echo "=== Building tb_eaglesong_permutation"
 iverilog $IVERILOG_ARGS -o ./tb_eaglesong_permutation.iv_sim.vvp ./rtl/eaglesong_permutation.sv ./tb/tb_eaglesong_permutation.sv
 echo "iverilog exit code: $?"
